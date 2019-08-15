26b5f753fc8ffcae1fac85113627de5b0c7f1ada
techyaura <techyaura@gmail.com>
2019-08-15 19:11:20 +0530

    Add authorized level in update & delete todo

--- src/services/todo/todo.service.js
+++ src/services/todo/todo.service.js
@@ -21,13 +21,27 @@ class TodoService {
   updateTodo(user, todoId, postBody) {
     return this.TodoModel.updateOne({
       user: user._id, isDeleted: false, status: true, _id: todoId
-    }, { $set: postBody });
+    }, { $set: postBody })
+      .then((response) => {
+        if (response && response.n !== 0) {
+          return Promise.resolve(response);
+        }
+        return Promise.reject(new Error('You are not authorized to update'));
+      })
+      .catch(err => Promise.reject(err));
   }
 
   deleteTodo(user, params) {
     return this.TodoModel.deleteOne({
       user: user._id, isDeleted: false, status: true, _id: params.id
-    });
+    })
+      .then((response) => {
+        if (response && response.n !== 0) {
+          return Promise.resolve(response);
+        }
+        return Promise.reject(new Error('You are not authorized to delete'));
+      })
+      .catch(err => Promise.reject(err));
   }
 }
 
