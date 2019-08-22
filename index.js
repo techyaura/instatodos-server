bb613df43af0c45ff4b1f8a3cd73e0372f671e55
techyaura <techyaura@gmail.com>
2019-08-22 16:11:59 +0530

    Update express root file to ES6

--- index.js
+++ index.js
@@ -4,52 +4,114 @@ require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`
 });
 
-// Express config
+class Boot {
+  constructor(dbConnection, app, cors, morgan, winston, schema, graphqlHTTP, AuthMiddleware) {
+    this.dbConnection = dbConnection;
+    this.app = app;
+    this.cors = cors;
+    this.port = process.env.PORT || 8080;
+    this.morgan = morgan;
+    this.winston = winston;
+    this.schema = schema;
+    this.graphqlHTTP = graphqlHTTP;
+    this.AuthMiddleware = AuthMiddleware;
+    this.boostrapExpress();
+  }
+
+  /**
+   * @name boostrapExpress
+   * @description Starting bootsrap server
+   */
+  async boostrapExpress() {
+    try {
+      await this.dbConnection();
+      return Promise.all([
+        this.useCors(),
+        this.useMorgan(),
+        this.useGraphQl(),
+        this.useErrors(),
+        this.useListen()
+      ]).then(() => {
+        console.log(`Running a GraphQL API server at localhost:${this.port}/graphql`); // eslint-disable-line no-console
+        return this.app;
+      });
+    } catch (err) {
+      console.log('Unable to start GraphQL API server'); // eslint-disable-line no-console
+      return err;
+    }
+  }
+
+  /**
+   * @name useCors
+   * @description use CORS policy
+   */
+  useCors() {
+    return this.app.use(this.cors());
+  }
+
+  /**
+   * @name useMorgan
+   * @description For logging
+   */
+  useMorgan() {
+    return this.app.use(this.morgan('combined', { stream: this.winston.stream }));
+  }
+
+  /**
+   * @name graphQlHttpConfig
+   * @description Graphql config
+   */
+  graphQlHttpConfig() {
+    return this.graphqlHTTP({
+      schema: this.schema,
+      pretty: true,
+      graphiql: true
+    });
+  }
+
+  /**
+   * @name useGraphQl
+   * @description configure express with gRaphql
+   */
+  useGraphQl() {
+    return this.app.use('/graphql', this.AuthMiddleware.jwt, this.graphQlHttpConfig());
+  }
+
+  /**
+   * @name useErrors
+   * @description Express Global Error Handler
+   */
+  useErrors() {
+    return this.app.use((err, req, res, next) => {
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get('env') === 'development' ? err : {};
+      // add this line to include winston logging
+      this.winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+      const errorHandler = require('./src/errors/handler');
+      return errorHandler(err, req, res, next);
+    });
+  }
+
+  /**
+   * @name useListen
+   * @description Express expose port for server
+   */
+  useListen() {
+    return this.app.listen(this.port);
+  }
+}
+
 const express = require('express');
+
+const app = express();
 const graphqlHTTP = require('express-graphql');
 const morgan = require('morgan');
 const cors = require('cors');
-
-// Use of graphql-tools for writing GraphQL Schema (RECOMMENDED)
+const dbConnection = require('./src/config/db');
 const schema = require('./src/gql');
-
-const port = process.env.PORT || 8080;
-
-// connect to the database
-require('./src/config/db');
-
 const winston = require('./src/config/winston');
-
-
 const { AuthMiddleware } = require('./src/middlewares');
 
-const errorHandler = require('./src/errors/handler');
-
-const app = express();
-
-// Configuring CORS
-app.use(cors());
-
-app.use(morgan('combined', { stream: winston.stream }));
-app.use('/graphql', AuthMiddleware.jwt, graphqlHTTP({
-  schema,
-  pretty: true,
-  graphiql: true
-}));
-
-app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // add this line to include winston logging
-  winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
-
-  return errorHandler(err, req, res, next);
-});
-
-
-app.listen(port);
-
-// custom console
-console.log(`Running a GraphQL API server at localhost:${port}/graphql`); // eslint-disable-line no-console
+const boot = new Boot(dbConnection, app, cors, morgan, winston, schema, graphqlHTTP, AuthMiddleware);
+module.exports = boot;
--- src/config/db.js
+++ src/config/db.js
@@ -1,23 +1,30 @@
-const mongoose = require('mongoose');
 
-// Create the database connection
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+module.exports = () => {
+  const mongoose = require('mongoose');
+  // Create the database connection
+  mongoose.Promise = global.Promise;
 
-// CONNECTION EVENTS
+  return new Promise((resolve, reject) => {
+    mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+    // CONNECTION EVENTS
 
-// When successfully connected
-mongoose.connection.on('connected', () => {
-  /* eslint no-console: 0 */
-  console.log(`Mongoose default connection open to ${process.env.DB_URL}`);
-});
+    // When successfully connected
+    mongoose.connection.on('connected', () => {
+    /* eslint no-console: 0 */
+      console.log(`Mongoose default connection open to ${process.env.DB_URL}`);
+      return resolve();
+    });
 
-// If the connection throws an error
-mongoose.connection.on('error', (err) => {
-  console.log(`Mongoose default connection error: ${err}`);
-});
+    // If the connection throws an error
+    mongoose.connection.on('error', (err) => {
+      console.log(`Mongoose default connection error: ${err}`);
+      return reject(err);
+    });
 
-// When the connection is disconnected
-mongoose.connection.on('disconnected', () => {
-  console.log('Mongoose default connection disconnected');
-});
+    // When the connection is disconnected
+    mongoose.connection.on('disconnected', () => {
+      console.log('Mongoose default connection disconnected');
+      return reject(new Error('Mongoose default connection disconnected'));
+    });
+  });
+};
