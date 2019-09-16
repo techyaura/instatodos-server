bd9faf347a2114205ba66e8ec500afaff900a8ce
techyaura <techyaura@gmail.com>
2019-09-16 20:51:24 +0530

    fixed optional check for label

--- src/middlewares/todo.middlewares.js
+++ src/middlewares/todo.middlewares.js
@@ -3,13 +3,16 @@ const { TodoLabelModel } = require('../models');
 module.exports = {
   checkLabel: (context, postBody) => {
     const { next } = context;
-    return TodoLabelModel.findOne({ _id: postBody.label })
-      .then((label) => {
-        if (label) {
-          return next();
-        }
-        return Promise.reject(new Error('Invalid Todo Label'));
-      });
+    if (postBody.label && typeof postBody.label !== 'undefined') {
+      return TodoLabelModel.findOne({ _id: postBody.label })
+        .then((label) => {
+          if (label) {
+            return next();
+          }
+          return Promise.reject(new Error('Invalid Todo Label'));
+        });
+    }
+    return next();
   }
 
 
