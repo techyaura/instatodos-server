f24075643c8f33447d27f33e9bc641e4c087b860
techyaura <techyaura@gmail.com>
2020-09-06 03:18:26 +0530

    fix google login issue

--- src/gql/resolvers/social-login.resolvers.js
+++ src/gql/resolvers/social-login.resolvers.js
@@ -3,7 +3,7 @@ const { SocialLoginService } = require('../../services');
 const mutations = {
   googleLogin: async (_, args) => {
     try {
-      await SocialLoginService.googleLogin(args.input);
+      return SocialLoginService.googleLogin(args.input);
     } catch (err) {
       throw err;
     }
