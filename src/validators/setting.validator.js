1e4e8fc68e1c522de649fe75d904934c9c25710d
techyaura <techyaura@gmail.com>
2021-06-14 13:20:00 +0530

    add config parameters as optional

--- src/validators/setting.validator.js
+++ src/validators/setting.validator.js
@@ -3,12 +3,10 @@ const Joi = require('joi');
 const configSchema = Joi.object().keys({
   theme: Joi.string()
     .min(3)
-    .max(30)
-    .required(),
+    .max(30),
   lang: Joi.string()
     .min(2)
     .max(2)
-    .required()
 });
 
 module.exports = {
